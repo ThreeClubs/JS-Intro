6fedf7eebd223f1912b3e3d341bb4c8b7d6729af
Andrew Grenon <andrew.grenon@gmail.com>
2022-08-30 12:54:33 +1200

    update 1

--- app.js
+++ app.js
@@ -1,38 +1,57 @@
-// console.log("Hello, everyone! This an external JS link");
+// // console.log("Hello, everyone! This an external JS link");
 
-let instructor = "Reuben!";
-let greeting = "Hello, ";
+// let instructor = "Reuben!";
+// let greeting = "Hello, ";
 
-const firstName = "Andrew";
-console.log(firstName);
+// const firstName = "Andrew";
+// console.log(firstName);
 
-function greetInstructor() {
-  let fullGreeting = greeting + instructor;
-  let myGreeting = "My name is " + firstName + ".";
-  alert(fullGreeting + " " + myGreeting);
+// function greetInstructor() {
+//   let fullGreeting = greeting + instructor;
+//   let myGreeting = "My name is " + firstName + ".";
+//   alert(fullGreeting + " " + myGreeting);
+// }
+
+// // let lastName;
+
+// // lastName = "Grenon";
+
+// // firstName = "Burt", lastName = "Munro";
+// // console.log(firstName, lastName);
+
+// const fullName = "Andrew Grenon"; //test single line comment
+// let age = 37; //my first variable
+
+// console.log(fullName);
+
+// // const ageCalc = 2022-1985;
+
+// let year = 2022;
+
+// const ageCalc = year - 1985;
+
+// console.log(fullName, age, ageCalc); //we can perform operations on declared variables as long as they are intergers
+
+// console.log((year * age) / year);
+
+const greetingHi= "Hi"
+let firstName = "";
+
+function hiJames() {
+  firstName = "James";
+  assembleGreeting = greetingHi + ", " + firstName + "!";
+  console.log(assembleGreeting);
 }
 
-// let lastName;
+hiJames();
 
-// lastName = "Grenon";
 
-// firstName = "Burt", lastName = "Munro";
-// console.log(firstName, lastName);
 
-const fullName = "Andrew Grenon"; //test single line comment
-let age = 37; //my first variable
 
-console.log(fullName);
 
-// const ageCalc = 2022-1985;
 
-let year = 2022;
 
-const ageCalc = year - 1985;
 
-console.log(fullName, age, ageCalc); //we can perform operations on declared variables as long as they are intergers
-
-console.log((year * age) / year);
 
 
 
